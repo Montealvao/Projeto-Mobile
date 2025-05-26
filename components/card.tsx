f0438f2ffa1e95ea27b1951fa21604f11d902a72
gcosta <gcosta@grupoimagetech.com.br>
2025-05-26 15:22:23 -0400

    feat: :sparkles: add components

---
+++ components/card.tsx
@@ -0,0 +1,19 @@
+// components/ui/card.tsx
+import { View, Text, ViewProps } from 'react-native';
+
+export function Card({ className = '', ...props }: ViewProps & { className?: string }) {
+  return (
+    <View
+      className={`rounded-2xl border border-light-300 bg-secondary p-4 shadow-sm ${className}`}
+      {...props}
+    />
+  );
+}
+
+export function CardTitle({ children, className = '' }: { children: React.ReactNode; className?: string }) {
+  return <Text className={`text-xl font-semibold text-light-100 ${className}`}>{children}</Text>;
+}
+
+export function CardDescription({ children, className = '' }: { children: React.ReactNode; className?: string }) {
+  return <Text className={`text-sm text-light-300 ${className}`}>{children}</Text>;
+}
