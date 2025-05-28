b3804744269d2b8e81118eff1db2550760cba356
____x_ <josuealexandre067@gmail.com>
2025-05-28 12:48:24 -0400

    feat: chat.tsx (frontend)

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,8 +1,21 @@
-import { Text } from "react-native";
-
+import { View, Text, ScrollView } from "react-native";
+import { Card, CardTitle, CardDescription } from '../../components/chat/card.tsx';
 
 export default function Chat() {
     return (
-        <Text>OI</Text>
+        <View className="flex-1 relative bg-white">
+            <View className="p-4 w-full">
+                <Text className="text-2xl font-bold">Conversas</Text>
+            </View>
+
+            <ScrollView>
+                <View className="flex-1 w-full h-full p-4">
+                    <Card className="mb-4">
+                        <CardTitle>Prof. João (Matemática)</CardTitle>
+                        <CardDescription>A prova será na próxima sexta</CardDescription>
+                    </Card>
+                </View>
+            </ScrollView>
+        </View>
     )
 }
